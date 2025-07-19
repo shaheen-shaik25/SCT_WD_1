13e317c975dad8573b044b6394f1c177806ef26b
Shaik Shaheen <shaikfathima08390@gmail.com>
2025-07-19 19:15:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change navbar style on scroll
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 20) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
